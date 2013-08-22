341c90ae7dc3f64de26290f371cda7341557b912
veryrobert <hello@robertfarrelly.com>
2013-08-22 18:05:49 +0100

    Another update for the files

--- javascript/plugins.js
+++ javascript/plugins.js
@@ -1,12 +1,21 @@
 
-
+$('.image-grid').masonry({
+  columnWidth: 200,
+  itemSelector: '.grid-image'
+});
 
 
 $("#trigger").click(function (){
-
 	$('#trigger').toggleClass('clicked');
 	$("#container").slideToggle('slow');
 	$(".network-list").delay(500).fadeToggle('slow');
 });
 
+// $('#project-excerpt').hide();	
+
+// $(".info-open").click(function (){
+// 	// $('.info-open').toggleClass('clicked');
+// 	$("#project-excerpt").fadeToggle('slow');
+// 	// $('.featured-image').toggleClass('clicked');
+// });
 
